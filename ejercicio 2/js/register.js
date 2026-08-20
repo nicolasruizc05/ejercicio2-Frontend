b96e611f42d05d365e90353014473c29716625e6
[200~benavid3es-lgtm <benavid3es@gmail.com>
2026-08-20 17:35:17 -0500

    Descripción de los cambios

--- ejercicio 2/js/register.js
+++ ejercicio 2/js/register.js
@@ -1,5 +1,5 @@
 function guardar() {
-    // 1. Capturar los datos tal cual los ingresa el usuario
+    // Capturar los datos tal cual los ingresa el usuario
     const nombre = document.getElementById("nombre").value;
     const apellido = document.getElementById("apellido").value;
     const cargo = document.getElementById("cargo").value;
@@ -7,23 +7,18 @@ function guardar() {
     const correo = document.getElementById("correo").value;
     const password = document.getElementById("password").value;
 
-    // 2. Validar que los campos no estén vacíos
+    //  Validar que los campos no estén vacíos
     if (nombre === "" || apellido === "" || area === "" || correo === "" || password === "") {
         alert("Por favor completa todos los campos.");
         return;
     }
 
-    // 3. Traer los usuarios que ya están en LocalStorage (si no hay ninguno, inicia [])
+    // Traer los usuarios que ya están en LocalStorage (si no hay ninguno, inicia [])
     const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
-    // 4. Validar si el correo ya existe
-    const correoExiste = usuarios.some(user => user.correo === correo);
-    if (correoExiste) {
-        alert("Este correo ya está registrado.");
-        return;
-    }
+   
 
-    // 5. Crear el objeto del nuevo usuario
+    //Crear el objeto del nuevo usuario
     const nuevoUsuario = {
         id: Date.now(),
         nombre: nombre,
@@ -34,7 +29,7 @@ function guardar() {
         password: password
     };
 
-    // 6. Agregar al arreglo y guardar en LocalStorage
+    // Agregar al arreglo y guardar en LocalStorage
     usuarios.push(nuevoUsuario);
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
